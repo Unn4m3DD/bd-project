627ac2d3fb2eff285ab5815ce1552c6565e4ac18
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-05-29 12:44:47 +0100

    change event list to arrays

--- src/api-endpoint.ts
+++ src/api-endpoint.ts
@@ -34,7 +34,7 @@ async function get_events(
       [start_time, end_time, quadtree_start, quadtree_end]
     ))
   }
-  
+
   if (station_id) {
     return await (query(
       `call get_${event_type}s_station_id(?, ?, ?);`,
@@ -56,6 +56,32 @@ const api_response: { [key: string]: (req: express.Request, res: express.Respons
   min_simultaneous_people_count: undefined,
   min_simultaneous_car_count: undefined,
   max_simultaneous_car_count: undefined,
+  event_timestamps: async (req, res) => {
+    /*
+    req start_time=1620223705
+    req end_time=1620223708
+    */
+    let start_time: number, end_time: number
+    if (req.query.time_interval) {
+      start_time = JSON.parse(req.query.time_interval as string)[0]
+      end_time = JSON.parse(req.query.time_interval as string)[1]
+    }
+    let timestamps;
+    if (start_time && end_time)
+      timestamps = (await query(`call get_timestamps_between(?, ?)`, [start_time, end_time]))[0]
+    else
+      timestamps = (await query(`call get_timestamps()`, []))[0]
+    timestamps = timestamps.map(e => e.event_timestamp)
+
+    let result = []
+    let max = 0
+    for (let i = 0; i < timestamps.length; i += 2) {
+      if ((timestamps[i + 1] - timestamps[i]) > 30)
+        result.push([timestamps[i], timestamps[i + 1]])
+    }
+    res.send(result)
+
+  },
   cams_list: async (req, res) => {
     /*
     req start_time=1620223705
@@ -75,17 +101,20 @@ const api_response: { [key: string]: (req: express.Request, res: express.Respons
     if (req.query.quadtree_zoom)
       zoom = JSON.parse(req.query.quadtree_zoom as string)
 
-    const vam_raw = await get_events("vam", start_time, end_time, number_quadtree, zoom, station_id)
-    const vam = {}
-    vam_raw[0].forEach(element => {
-      vam[element.timestamp] = {
+    const cam_raw = await get_events("vam", start_time, end_time, number_quadtree, zoom, station_id)
+    const cam = {}
+    cam_raw[0].forEach(element => {
+      if (!cam[element.timestamp])
+        cam[element.timestamp] = []
+      cam[element.timestamp].push({
         station_id: element.station_id,
         longitude: element.longitude,
         latitude: element.latitude,
+        station_type: element.station_type,
         perceived_objects: JSON.parse(element.perceived_objects),
-      }
+      })
     })
-    res.send(vam)
+    res.send(cam)
   },
   notifications_list: undefined,
   events: async (req, res) => {
@@ -110,37 +139,43 @@ const api_response: { [key: string]: (req: express.Request, res: express.Respons
     const cpm_raw = await get_events("cpm", start_time, end_time, number_quadtree, zoom, station_id)
     const cpm = {}
     cpm_raw[0].forEach(element => {
-      cpm[element.timestamp] = {
+      if (!cpm[element.timestamp])
+        cpm[element.timestamp] = []
+      cpm[element.timestamp].push({
         station_id: element.station_id,
         longitude: element.longitude,
         latitude: element.latitude,
         perceived_objects: JSON.parse(element.perceived_objects),
-      }
+      })
     })
 
     const cam_raw = await get_events("cam", start_time, end_time, number_quadtree, zoom, station_id)
     const cam = {}
     cam_raw[0].forEach(element => {
-      cam[element.timestamp] = {
+      if (!cam[element.timestamp])
+        cam[element.timestamp] = []
+      cam[element.timestamp].push({
         station_id: element.station_id,
         longitude: element.longitude,
         latitude: element.latitude,
         station_type: element.station_type,
         perceived_objects: JSON.parse(element.perceived_objects),
-      }
+      })
     })
 
 
     const vam_raw = await get_events("vam", start_time, end_time, number_quadtree, zoom, station_id)
     const vam = {}
     vam_raw[0].forEach(element => {
-      vam[element.timestamp] = {
+      if (!vam[element.timestamp])
+        vam[element.timestamp] = []
+      vam[element.timestamp].push({
         station_id: element.station_id,
         longitude: element.longitude,
         latitude: element.latitude,
         station_type: element.station_type,
         perceived_objects: JSON.parse(element.perceived_objects),
-      }
+      })
     })
 
     let response = {};
@@ -192,12 +227,14 @@ const api_response: { [key: string]: (req: express.Request, res: express.Respons
     const cpm_raw = await get_events("cpm", start_time, end_time, number_quadtree, zoom, station_id)
     const cpm = {}
     cpm_raw[0].forEach(element => {
-      cpm[element.timestamp] = {
+      if (!cpm[element.timestamp])
+        cpm[element.timestamp] = []
+      cpm[element.timestamp].push({
         station_id: element.station_id,
         longitude: element.longitude,
         latitude: element.latitude,
         perceived_objects: JSON.parse(element.perceived_objects),
-      }
+      })
     })
     res.send(cpm)
   },
@@ -223,12 +260,15 @@ const api_response: { [key: string]: (req: express.Request, res: express.Respons
     const vam_raw = await get_events("vam", start_time, end_time, number_quadtree, zoom, station_id)
     const vam = {}
     vam_raw[0].forEach(element => {
-      vam[element.timestamp] = {
+      if (!vam[element.timestamp])
+        vam[element.timestamp] = []
+      vam[element.timestamp].push({
         station_id: element.station_id,
         longitude: element.longitude,
         latitude: element.latitude,
+        station_type: element.station_type,
         perceived_objects: JSON.parse(element.perceived_objects),
-      }
+      })
     })
     res.send(vam)
   },
