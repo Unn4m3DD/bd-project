ae05e0dbd57214e495123c069b630fde8e7abd86
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-05-29 12:42:56 +0100

    changed latitude with longitude and added index on timestamp

---
+++ database/mariadb/change_lat_lon.sql
@@ -0,0 +1,15 @@
+UPDATE CPM
+SET latitude = longitude, 
+    longitude = latitude; 
+
+UPDATE CAM
+SET latitude = longitude, 
+    longitude = latitude; 
+
+UPDATE VAM
+SET latitude = longitude, 
+    longitude = latitude; 
+
+UPDATE DENM
+SET latitude = longitude, 
+    longitude = latitude; 
---
+++ database/mariadb/indexes.sql
@@ -0,0 +1,4 @@
+create index timestamp_cpm on CPM(event_timestamp);
+create index timestamp_cpm on CAM(event_timestamp);
+create index timestamp_cpm on VAM(event_timestamp);
+create index timestamp_cpm on DENM(event_timestamp);
