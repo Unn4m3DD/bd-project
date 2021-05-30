0535134a0fc4a444e51c435bf497f94780a42a20
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-05-30 14:20:58 +0000

    new query function

--- src/api-endpoint.ts
+++ src/api-endpoint.ts
@@ -3,9 +3,9 @@ import express = require('express')
 
 import api from "./api-compiled"
 const app = express()
-let query: (sql_query: string, query_parameters: any[]) => Promise<any>
+let query: (procedure_name: string, query_parameters: any[]) => Promise<any>
 
-export function setupAPIEndpoint(outer_query: (sql_query: string, query_parameters: any[]) => Promise<any>) {
+export function setupAPIEndpoint(outer_query: (procedure_name: string, query_parameters: any[]) => Promise<any>) {
   query = outer_query
   setup()
 }
